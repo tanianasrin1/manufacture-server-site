a98c0a1413592c5345e3787040da562512877d61
tanianasrin1 <tasrineva169@gmail.com>
2022-05-25 12:32:04 +0600

    dynamic id  page fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,37 @@ const port = process.env.PORT || 5000;
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oxkyd.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const serviceCollection = client.db('leptop_tools').collection('services');
+
+        app.get('/service', async(req, res)=> {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+
+        app.get('/service/:serviceId', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello leptop tools')
