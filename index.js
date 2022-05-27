aaa2a0f15d3eeccc5db7623968a2f7658b6a3e4e
tanianasrin1 <tasrineva169@gmail.com>
2022-05-27 18:04:17 +0600

    admin  page fixed

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'UnAuthorized access'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    if(err){
+      return res.status(403).send({message: 'Forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oxkyd.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -21,6 +39,7 @@ async function run(){
         const serviceCollection = client.db('leptop_tools').collection('services');
         const userCollection = client.db('leptop_tools').collection('users');
         const bookCollection = client.db('leptop_tools').collection('books');
+        const reviewCollection = client.db('leptop_tools').collection('reviews');
 
         app.get('/service', async(req, res)=> {
             const query = {};
@@ -44,14 +63,66 @@ async function run(){
            const result=  await bookCollection.insertOne(book) 
            res.send(result);
            
-         })
+         });
+
+        //  add review
+        app.post('/review',  async(req, res)=> {
+          const review = req.body;
+          const result=  await reviewCollection.insertOne(review) 
+          res.send(result);
+          
+        });
+
+          // add email
+        app.get('/review/:email', async(req,res)=> {
+          const email = req.params.email;
+          const query = {email: email};
+          const result = await reviewCollection.find(query).toArray();
+          res.send(result);
+        });
+
+        // get review
+        app.get('/reviews', async(req, res) => {
+          const users = await reviewCollection.find().toArray();
+          res.send(users);
+        })
+
+      //  admin
+
+      app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        const email = req.params.email;
+        const requester = req.decoded?.email;
+        const requesterAccount = await userCollection.findOne({email: requester});
+        if(requesterAccount.role === 'admin'){
+          const filter = { email: email };
+          const updateDoc = {
+            $set: {role: 'admin'},
+           
+          };
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.send(result);
+        }
+        else{
+          res.status(403).send({message: 'forbidden'});
+        }
+     
+      });
+
+      // admin
+      app.get('/admin/:email', async(req, res) => {
+        const email = req.params.email;
+        const user = await userCollection.findOne({email: email});
+        const isAdmin = user.role === 'admin';
+        res.send({admin: isAdmin});
+      })
+
 
         app.get('/user', async(req, res) => {
           const users = await userCollection.find().toArray();
           res.send(users);
         })
 
-        app.put('/user/admin/:email', async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT,  async (req, res) => {
           const email = req.params.email;
           const filter = {email: email};
           const updateDoc = {
@@ -61,6 +132,14 @@ async function run(){
           const result = await userCollection.updateOne(filter, updateDoc);
           res.send(result);
       
+        })
+
+        // admin
+        app.delete('/userDelete/:email', verifyJWT, async(req, res)=>{
+          const email = req.params.email;
+          const query = {email: email};
+          const result = await userCollection.deleteOne(query);
+          res.send(result);
         })
 
       
@@ -73,6 +152,17 @@ async function run(){
         });
 
 
+        
+
+        // delete booking
+        app.delete('/deleteBooking/:id', async(req, res)=>{
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+          const result = await bookCollection.deleteOne(query);
+          res.send(result);
+        })
+
+
         app.put('/user/:email', async (req, res) => {
           const email = req.params.email;
           const user = req.body;
@@ -83,8 +173,8 @@ async function run(){
           }
 
           const result = await userCollection.updateOne(filter, updateDoc, options);
-          const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'})
-          res.send({result, token});
+          const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+          res.send({result, accessToken: token});
         })
 
     }
